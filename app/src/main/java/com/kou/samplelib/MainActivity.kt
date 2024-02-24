93ecd85481aa98771043f96e3dd69d4d221dcd39
hwang.inchul <hwang.inchul@nhn-comico.com>
2024-02-25 00:01:23 +0900

    Initial commit

--- app/src/main/java/com/kou/samplelib/MainActivity.kt
+++ app/src/main/java/com/kou/samplelib/MainActivity.kt
@@ -2,10 +2,12 @@ package com.kou.samplelib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kou.mylibrary.ShowLib
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ShowLib.shortToast(this, "TEST")
     }
 }
